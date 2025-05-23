e3a8a5a686117fca4cef0e2695e505b646814683
OWO Devs <developers.owo@gmail.com>
2025-05-23 12:37:05 +0200

    build: mod runs owoskin

--- OWO_Valheim.cs
+++ OWO_Valheim.cs
@@ -1,13 +1,31 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
 
 namespace OWO_Valheim
 {
     [BepInPlugin("org.bepinex.plugins.OWO_Valheim", "OWO_Valheim", "1.0.0")]
-    public class Plugin
+    public class Plugin : BaseUnityPlugin
     {
 #pragma warning disable CS0109
         internal static new ManualLogSource Log;
 #pragma warning restore CS0109
+
+        public static OWOSkin owoSkin;
+
+
+        private void Awake()
+        {
+            Log = Logger;
+            Logger.LogMessage("OWO_Valheim plugin is loaded!");
+
+            owoSkin = new OWOSkin();
+
+            var harmony = new Harmony("owo.patch.valheim");
+            harmony.PatchAll();
+        }
     }
 }
